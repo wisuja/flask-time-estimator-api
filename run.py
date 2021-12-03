219e7da192644f008c23eda0c913366d5d9f1f10
William Surya Jaya <williamsj26.wsj@gmail.com>
2021-12-03 17:31:27 +0700

    Update

--- run.py
+++ run.py
@@ -7,18 +7,29 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def home():
   model = pickle.load(open('finalized_model.sav', 'rb'))
+  scaler = pickle.load(open('scaler.sav', 'rb'))
 
   task_type = request.form.get('task_type')
   number_of_peoples = request.form.get('number_of_peoples')
 
-  df = pd.DataFrame({
+  originalRequest = {
     'task_type': [task_type],
-    'number_of_peoples': [number_of_peoples]
-  })
+    'number_of_peoples': [number_of_peoples],
+    'estimated_time_in_minutes': [0]
+  }
+
+  df = pd.DataFrame.from_dict(originalRequest)
+  df[list(df.columns)] = scaler.transform(df)
+  df = df.drop(columns='estimated_time_in_minutes')
 
   prediction = model.predict(df).tolist()
+  df['estimated_time_in_minutes'] = prediction
+
+  scaledPrediction = scaler.inverse_transform(df)
+  finalPrediction = scaledPrediction[0, 2]
+
   response = {
-    'prediction': prediction
+    'prediction': finalPrediction
   }
 
   return jsonify(response)
